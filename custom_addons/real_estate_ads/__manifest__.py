bbcfbe061855f1b6ebf3ef3349ebe4d6d2af28d3
HasanArnous <hasan.arnous.andriome@gmail.com>
2023-12-10 17:55:05 +0300

    Initial files for the custom module "Real State Ads"

---
+++ custom_addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Real State Ads",
+    "version": "1.0",
+    "author": "HASyriAN",
+    "website": "https://www.orait.com.sa",
+    "description": """
+        Real State Advertisement, a custom addon for Odoo
+    """,
+    "application": True,
+    "installable": True,
+    "category": "Sales",
+    "depends": [],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/property_view.xml",
+        "views/menu_items.xml",
+    ],
+    "demo": [],
+    "license": "LGPL-3"
+}
---
+++ custom_addons/real_estate_ads/models/property.py
@@ -0,0 +1,30 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    # THE NAME & DESCRIPTION OF THE TABLE IN THE DB (MANDATORY)
+    _name = "estate.property"
+    _description = "Real Estate Model"
+
+    # HERE WE ADD THE FIELDS
+    # we can use something like 'readonly' and other parameters as well
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description", required=True)
+    post_code = fields.Char(string="Post Code")
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    best_offer = fields.Float(string="Best Offer")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area(sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area")
+    # SELECTION TAKE A LIST Tuples '()', not Maps/Sets '{}'
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'),
+                                           ('west', 'West'), ('east', 'East')],
+                                          string="Garden Orientation", default=('east', 'East'))
+
+    # Odoo will generate some fields automatically like the following:
+    # id, create_date, create_uid, write_date, write_uid
