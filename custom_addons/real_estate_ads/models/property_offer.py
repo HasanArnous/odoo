a43574dde9359ba93129179f982a7aaf2e4a106e
HasanArnous <hasan.arnous.andriome@gmail.com>
2023-12-14 17:22:23 +0300

    Real Estate Ads --> property_offer.py: Updated the PropertyOffer model by adding the validity & the deadline fields.

--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -5,7 +5,9 @@ class PropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Property Offers"
 
-    price = fields.Float(string="price")
+    price = fields.Float(string="Price")
     status = fields.Selection([('Accepted', 'accepted'), ('Refused', 'refused')], string="Status")
     partner_id = fields.Many2one('res.partner', string="Customer")
     property_id = fields.Many2one('estate.property', string="Property")
+    validity = fields.Integer(string="Validity")
+    deadline = fields.Date(stirng="Deadline")
