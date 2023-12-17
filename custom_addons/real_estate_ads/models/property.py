8c0809bb0a5ef2438ca5c614e76a136915de2814
HasanArnous <hasan.arnous.andriome@gmail.com>
2023-12-17 11:01:09 +0300

    real_estate_ads --> property.py + property_offer.py + property_view.xml:  - Added a new field in the Property model 'total_area' and used a compute method to compute the total area depending on the living_area & garden_area fields.  - Showed how to use the onchange method to compute a field automatically in the context of the FORM!.  - Added a new field 'creation_date' in the PropertyOffer model, and used the compute method to compute the deadline value on the fly, and the inverse method to calculate the value of the validity from the deadline and creation_date.

--- custom_addons/real_estate_ads/models/property.py
+++ custom_addons/real_estate_ads/models/property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Property(models.Model):
@@ -30,6 +30,19 @@ class Property(models.Model):
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
     sales_id = fields.Many2one("res.users", string="Salesman")
     buyer_id = fields.Many2one("res.partner", string="Buyer")
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for rec in self:
+            rec.total_area = rec.living_area + rec.garden_area
+
+    total_area = fields.Integer(string="Total Area", compute=_compute_total_area)
+    # or we can the name of the method in the compute parameter as string => compute="_compute_total_area"
+
+    # There is also the onchange method, the onchange method will be triggered on the level of form context
+    # @api.onchange('living_area', 'garden_area')
+    # def _onchange_total_area(self):
+    #     self.total_area = self.living_area + self.garden_area
 
     # Odoo will generate some fields automatically like the following:
     # id, create_date, create_uid, write_date, write_uid
--- custom_addons/real_estate_ads/models/property_offer.py
+++ custom_addons/real_estate_ads/models/property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import datetime
 
 
 class PropertyOffer(models.Model):
@@ -6,8 +7,22 @@ class PropertyOffer(models.Model):
     _description = "Property Offers"
 
     price = fields.Float(string="Price")
-    status = fields.Selection([('Accepted', 'accepted'), ('Refused', 'refused')], string="Status")
+    status = fields.Selection([('Accepted', 'accepted'), ('Refused', 'refused',)], string="Status")
+    creation_date = fields.Date(string="Creation Date", default=datetime.datetime.today())
     partner_id = fields.Many2one('res.partner', string="Customer")
     property_id = fields.Many2one('estate.property', string="Property")
     validity = fields.Integer(string="Validity")
-    deadline = fields.Date(stirng="Deadline")
+    deadline = fields.Date(stirng="Deadline", compute='_compute_deadline', inverse='_inverse_deadline')
+
+    @api.depends('creation_date', 'validity')
+    def _compute_deadline(self):
+        for rec in self:
+            if rec.creation_date and rec.validity:
+                rec.deadline = rec.creation_date + datetime.timedelta(days=rec.validity)
+            else:
+                rec.deadline = False
+
+    # It will be triggered only after saving/update the record
+    def _inverse_deadline(self):
+        for rec in self:
+            rec.validity = (rec.deadline - rec.creation_date).days
